a09201c0761c1dc1e88b3e543eae99ef1bbb9f8c
Abel <abelsena1@hotmail.com>
2022-08-29 19:55:28 -0300

    fix

--- src/app/containers/Timer/index.js
+++ src/app/containers/Timer/index.js
@@ -7,45 +7,50 @@ import ButtonSimples from '../../components/ButtonSimples';
 const Time = () => {
     const [valueInitial, setValueInitial] = useState(0)
     const [totalTimeInSeconds, setTotalTimeInSeconds] = useState(valueInitial * 60)
-    const [error, setError] = useState()
+    const [error, setError] = useState('')
 
+    
     const minutes = Math.floor(totalTimeInSeconds / 60)
     const seconds = totalTimeInSeconds % 60
 
     const onChangeTime = () => {
-        if(valueInitial === 0){
+        if(valueInitial == 0){
             setError("Preencha um Valor Diferente de 0!")
         }
-
-        if(valueInitial === ""){
+        if(valueInitial == ""){
             setError('Valor não Pode ser Vazio!')
         }
-       else{
+       if(valueInitial != 0){
         setTotalTimeInSeconds(valueInitial)
-           
        }
+       console.log("Valor do Initial", valueInitial)
     }
 
     const stop = () => {
+        clearInterval(minutes)
+        clearInterval(seconds)
         console.log("Parou")
     }
 
     useEffect(() => {
+        setError('')
         if(totalTimeInSeconds === 0){
             return
         }
         if(totalTimeInSeconds === ""){
             return
         }
-        else{
-            
+        
+        if(totalTimeInSeconds !== 0){
             setTimeout(() => {
+            if(totalTimeInSeconds !== 0){
                 setTotalTimeInSeconds(totalTimeInSeconds - 1)
+            }
             }, 1000)
-
+                    
         }
         
-    }, [totalTimeInSeconds, valueInitial])
+    }, [totalTimeInSeconds])
   
 
     return (
